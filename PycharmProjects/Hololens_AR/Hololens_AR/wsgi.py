5a90ccab9573aea6e7c2c429848db02a06e6055d
wudanqin <wdanqin>
2019-07-21 07:59:07 +0800

    ar/vr

---
+++ PycharmProjects/Hololens_AR/Hololens_AR/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for Hololens_AR project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Hololens_AR.settings')
+
+application = get_wsgi_application()
